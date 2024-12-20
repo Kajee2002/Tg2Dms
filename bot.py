d0a16e40e8776c77318f468262cd5e4f358955b6
kajee0311 <kajatheepan0311@gmail.com>
2024-12-20 13:55:24 +0530

    minor fix

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ app = Client(
 )
 
 #Start command
-@app.on_message(filters.command(["start"]))
+@app.on_message(filters.command("start"))
 async def start(client,message):
     await message.reply_text(
         text=Translation.START.format(name=message.from_user.first_name),
@@ -36,10 +36,16 @@ async def main():
         print("Client started. Listening for messages...")
         try:
           await asyncio.Event().wait()  # Keeps the client running
+        except KeyboardInterrupt:
+            print("Client stopped due to keyboard interupt.")
         except:
           print("Client stopped.")
 
 
 # Start the asyncio loop
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except:
+        print("An error occured.")
+        sys.exit(1)
