239946af1ae89904ca49a09f6613e1ce99e0bfbb
kajee0311 <kajatheepan0311@gmail.com>
2024-12-20 13:45:18 +0530

    minor fix

--- configs.py
+++ configs.py
@@ -1,16 +1,15 @@
-from google.colab import userdata as userdata
-
+import os
 
 class Configs(object):
     #Bot configurations
-    API_ID=int(userdata.get("API_ID"))
-    API_HASH=userdata.get("API_HASH")
-    BOT_TOKEN=userdata.get("BOT_TOKEN")
+    API_ID=int(os.environ.get("API_ID"))
+    API_HASH=os.environ.get("API_HASH")
+    BOT_TOKEN=os.environ.get("BOT_TOKEN")
     
     #Dms configurations
-    UserName=userdata.get("UserName")
-    Password=userdata.get("Password")
-    LoginDetail=f'userdata.get("UserName"):userdata.get("Password")'
+    UserName=os.environ.get("UserName")
+    Password=os.environ.get("Password")
+    LoginDetail=f'{os.environ.get("UserName")}:{os.environ.get("Password")}'
     Header = '"OCS-APIRequest: true"'
     UploadPoint = '"https://dms.uom.lk/remote.php/webdav/"'
     SharePoint = '"https://dms.uom.lk/ocs/v2.php/apps/files_sharing/api/v1/shares?format=json"'
